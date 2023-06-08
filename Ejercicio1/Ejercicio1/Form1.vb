edc63652613de4fa4a7ddcb73cb921369bcd8d28
DiegoMartin14 <diegodanielmartin@hotmail.com>
2023-06-08 20:50:38 -0300

    Ejercicio1

---
+++ Ejercicio1/Ejercicio1/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim j As Integer
+        Dim numero As Integer
+        Dim resultado As Double
+        numero = InputBox("Ingresar un numero natural para determinar su factorial")
+        resultado = 1
+        For j = 1 To numero
+            resultado = resultado * j
+        Next j
+        lblMostrar.Text = "" & resultado
+
+    End Sub
+End Class
